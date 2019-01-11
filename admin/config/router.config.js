0840174d8e4c261c1c15b7be3981bd2efcfde12f
Feng Yifei <fengyfei@gmail.com>
2019-01-11 10:12:24 +0800

    remove account settings

--- admin/config/router.config.js
+++ admin/config/router.config.js
@@ -204,18 +204,6 @@ export default [
                 path: '/account/settings/base',
                 component: './Account/Settings/BaseView',
               },
-              {
-                path: '/account/settings/security',
-                component: './Account/Settings/SecurityView',
-              },
-              {
-                path: '/account/settings/binding',
-                component: './Account/Settings/BindingView',
-              },
-              {
-                path: '/account/settings/notification',
-                component: './Account/Settings/NotificationView',
-              },
             ],
           },
         ],
